03b7fa9de9d9e361e0f5da728f39632d1f308a61
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-09-09 03:20:02 +0000

    fix problem task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,7 @@
 def multiple_returns(sentence):
     len_sen = len(sentence)
     first_char = sentence[0]
-    return (len_sen, first_char)
+    if not sentence:
+        return (len_sen, None)
+    else:
+        return (len_sen, first_char)
