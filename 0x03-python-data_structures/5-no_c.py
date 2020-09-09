89ac4cc29366dd882e5ae0cde810daddc5d00dc6
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-09-09 01:43:49 +0000

    try fix problem tasj 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     c_list = []
-    separator = ' '
     for i in my_string:
         if i != 'c' and i != 'C':
             c_list.append(i)
-    return separator.join(c_list)
+    return ' '.join(c_list)
