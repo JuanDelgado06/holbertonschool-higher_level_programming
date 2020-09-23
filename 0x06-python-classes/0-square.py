dcf7c5178f957e23f40e0b4cd0400f95edb99eaa
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-09-22 22:31:26 -0500

    add task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
     """Represents a square"""
-    pass
+    def __init__(self):
+        pass
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
