35d84d13eaeb82b2c5275cf183d810ca6fd59981
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-09-25 21:29:34 -0500

    add test task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,3 +14,20 @@ def add_integer(a, b=98):
     if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
+
+
+add_integer = __import__('0-add_integer').add_integer
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
+print(add_integer(True))
