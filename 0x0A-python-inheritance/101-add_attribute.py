8337ef6c30027bda307fc0028d185a1d5e283fb1
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-10-08 16:58:24 -0500

    add task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the add attribute module.
+"""
+
+
+def add_attribute(self, attribute, value):
+    """Add attribute if it's possible."""
+    if hasattr(self, '__dict__'):
+        setattr(self, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
