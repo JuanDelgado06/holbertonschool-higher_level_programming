2d8062b685d45a147cc7072870e31000f25c079c
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-10-13 17:28:24 -0500

    add update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,6 +39,23 @@ class Rectangle(Base):
         """Print the rectangle."""
         print('\n' * self.__y + (' ' * self.__x + '#' * self.__width + '\n') *
               self.__height, end='')
+    
+    def update(self, *args, **kwargs):
+        """Update the rectangle: id, width, height, x, y."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     @property
     def width(self):
