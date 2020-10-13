a012c95ace8cd40ebab7135fc61e59b189b6aaf3
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-10-13 14:08:02 -0500

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 
+Base Class
+"""
+
+
+class Base :
+    """ Base Class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize a Base"""
+        if id is  not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
